98d25685961a00ec36ac0742baa9836f3c3d6f5c
Hoàng Long <longhoangai1102@gmail.com>
2023-08-08 00:26:46 +0700

    Update run.py

--- run.py
+++ run.py
@@ -26,70 +26,70 @@ if __name__ == "__main__":
     parser.add_argument("--state_checkpoint", default=None, type=str)
     args = parser.parse_args()
 
-    # # Mixed precision
-    # mixed_precision = torch.float16 if torch.cuda.is_available() else torch.bfloat16
-    # ctx = autocast(dtype = mixed_precision)
-    # scaler = GradScaler()
+    # Mixed precision
+    mixed_precision = torch.float16 if torch.cuda.is_available() else torch.bfloat16
+    ctx = autocast(dtype = mixed_precision)
+    scaler = GradScaler()
 
-    # # ddp config
-    # backend = "nccl"
-    # init_process_group(backend = backend)
-    # local_rank = int(os.environ["LOCAL_RANK"])
+    # ddp config
+    backend = "nccl"
+    init_process_group(backend = backend)
+    local_rank = int(os.environ["LOCAL_RANK"])
 
-    # # Tokenizer and Model
-    # config = Config()
-    # tokenizer = config.tokenizer(model_checkpoint = args.model_checkpoint)
-    # if args.model_weigth_path:
-    #     lora_model = config.reload_pretrained_model(model_weight_path = args.model_weight_path, device_map = {"": torch.device(f"cuda:{local_rank}")})
-    # else:
-    #     model = config.load_pretrained_model(model_checkpoint = args.model_checkpoint, device_map = {"": torch.device(f"cuda:{local_rank}")})
-    #     lora_model = config.add_lora(model = model, r = 8, lora_alpha = 16, lora_dropout = 0.05)
+    # Tokenizer and Model
+    config = Config()
+    tokenizer = config.tokenizer(model_checkpoint = args.model_checkpoint)
+    if args.model_weigth_path:
+        lora_model = config.reload_pretrained_model(model_weight_path = args.model_weight_path, device_map = {"": torch.device(f"cuda:{local_rank}")})
+    else:
+        model = config.load_pretrained_model(model_checkpoint = args.model_checkpoint, device_map = {"": torch.device(f"cuda:{local_rank}")})
+        lora_model = config.add_lora(model = model, r = 8, lora_alpha = 16, lora_dropout = 0.05)
     
-    # # Dataset
-    # data_prcess = DataProcess(data_path = "MBZUAI/Bactrian-X", tokenizer = tokenizer)
-    # dataset = data_prcess.load_data()
-    # prompter = Prompter()
+    # Dataset
+    data_prcess = DataProcess(data_path = "MBZUAI/Bactrian-X", tokenizer = tokenizer)
+    dataset = data_prcess.load_data()
+    prompter = Prompter()
 
-    # splited_dataset = dataset.train_test_split(test_size = args.test_size, seed = 42)
+    splited_dataset = dataset.train_test_split(test_size = args.test_size, seed = 42)
 
-    # # Model inputs
-    # model_inputs = MODEL_INPUTS(prompter =  prompter,
-    #                             tokenizer = tokenizer,
-    #                             max_length = args.max_length)
+    # Model inputs
+    model_inputs = MODEL_INPUTS(prompter =  prompter,
+                                tokenizer = tokenizer,
+                                max_length = args.max_length)
     
-    # train_data = splited_dataset["train"].shuffle().map(model_inputs.generate_and_tokenize_prompt)
-    # valid_data = splited_dataset["test"].map(model_inputs.generate_and_tokenize_prompt)
+    train_data = splited_dataset["train"].shuffle().map(model_inputs.generate_and_tokenize_prompt)
+    valid_data = splited_dataset["test"].map(model_inputs.generate_and_tokenize_prompt)
 
-    # train_data = train_data.remove_columns(["instruction", "input", "id", "output"])
-    # valid_data = valid_data.remove_columns(["instruction", "input", "id", "output"])
+    train_data = train_data.remove_columns(["instruction", "input", "id", "output"])
+    valid_data = valid_data.remove_columns(["instruction", "input", "id", "output"])
 
-    # train_data.set_format("torch")
-    # valid_data.set_format("torch")
+    train_data.set_format("torch")
+    valid_data.set_format("torch")
 
-    # train_dataloader, valid_dataloader = model_inputs.prepare_dataloader(train_data,
-    #                                                                      valid_data,
-    #                                                                      batch_size = args.batch_size)
-    #  # Train
-    # trainer = Trainer(lr = 3e-4,
-    #                   epochs = args.epochs,
-    #                   model = lora_model,
-    #                   gradient_accumulation_steps = 4,
-    #                   gpu_id = local_rank,
-    #                   mixed_precision = mixed_precision,
-    #                   scaler = scaler,
-    #                   ctx = ctx)
+    train_dataloader, valid_dataloader = model_inputs.prepare_dataloader(train_data,
+                                                                         valid_data,
+                                                                         batch_size = args.batch_size)
+     # Train
+    trainer = Trainer(lr = 3e-4,
+                      epochs = args.epochs,
+                      model = lora_model,
+                      gradient_accumulation_steps = 4,
+                      gpu_id = local_rank,
+                      mixed_precision = mixed_precision,
+                      scaler = scaler,
+                      ctx = ctx)
     
-    # if args.state_checkpoint:
-    #     state_checkpoint = torch.load(args.state_checkpoint)
-    # else:
-    #     state_checkpoint = args.state_checkpoint
+    if args.state_checkpoint:
+        state_checkpoint = torch.load(args.state_checkpoint)
+    else:
+        state_checkpoint = args.state_checkpoint
 
-    # trainer.train(train_dataloader = train_dataloader,
-    #               valid_dataloader = valid_dataloader,
-    #               display_steps = args.display_steps,
-    #               save_steps = args.save_steps,
-    #               save_state_name = args.save_state_name,
-    #               save_model_name = args.save_model_name,
-    #               state_checkpoint = state_checkpoint)
+    trainer.train(train_dataloader = train_dataloader,
+                  valid_dataloader = valid_dataloader,
+                  display_steps = args.display_steps,
+                  save_steps = args.save_steps,
+                  save_state_name = args.save_state_name,
+                  save_model_name = args.save_model_name,
+                  state_checkpoint = state_checkpoint)
     
-    # destroy_process_group()
+    destroy_process_group()
