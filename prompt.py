d288ce0dde0272922b238854efa7c9af1aa6b9fa
Hoàng Long <longhoangai1102@gmail.com>
2023-11-22 14:10:35 +0700

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -6,7 +6,7 @@ class Prompter(object):
     def __init__(self, template_name: str = "", verbose: bool = False):
         self.template = {
             "prompt_input": "Dưới đây là một Instruction mô tả một nhiệm vụ, được ghép nối với một Input cung cấp thêm ngữ cảnh. Viết một Response hoàn thành yêu cầu một cách thích hợp.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n",
-            "prompt_no_input": "Dưới đây là một Instruction mô tả một nhiệm vụ. Việt một Response hoàn thành yêu cầu một cách thích hợp.\n\n### Instruction:\n{instruction}\n\n### Response:\n",
+            "prompt_no_input": "Dưới đây là một Instruction mô tả một nhiệm vụ. Viết một Response hoàn thành yêu cầu một cách thích hợp.\n\n### Instruction:\n{instruction}\n\n### Response:\n",
             "response_split": "### Response:"
         }
 
